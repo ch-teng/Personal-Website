0e40d812a48d8f8ae2db00d02f02451f03debab0
ch-teng <cteng601@gmail.com>
2022-02-13 12:50:28 -0500

    Fixed home module css name

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,34 @@
 
-import * as rain from "./rain.module.css"
+import * as home from "./home.module.css"
 import {Link} from "gatsby"
 
 
 //does not need to wrap layout manually - done through gatsby-browser.js
 const IndexPage = () => {
   return (
-    <div className={rain.container}>
-      <div className={rain.rain}>
-        <div className={rain.drop}></div>
-        {/* <div className={rain.waves}>
+    <div className={home.container}>
+      <div className={home.rain}>
+        <div className={home.drop}></div>
+        {/* <div className={home.waves}>
           <div></div>
           <div></div>
         </div> */}
-        <div className={rain.namecard}>
-          <p className={rain.name}>
+        <div className={home.namecard}>
+          <p className={home.name}>
             Chris Teng
           </p>
-          <p className={rain.quote}>
+          <p className={home.quote}>
             Do more with less
           </p>
         </div>
       </div>
-      <div className={rain.links}>
-        <a href="www.linkedin.com/in/ch-teng"><div className={rain.LinkedIn}>LinkedIn</div></a>
-        <a href= "https://github.com/ch-teng"><div className={rain.GitHub}>GitHub</div></a>
-        <Link to="/resume"><div className={rain.Resume}>Resume</div></Link>
-        <div className={rain.PersonalEmail} onClick={() => {navigator.clipboard.writeText("Chris.R.Teng@gmail.com"); alert("Chris.R.Teng@gmail.com has been copied to clipboard")}}>
-          <span className={rain.DisplayedEmail}>Personal Email</span> 
-          <span className={rain.EmailClicked}>Copied To Clipboard!</span>
+      <div className={home.links}>
+        <a href="https://www.linkedin.com/in/ch-teng" target="_blank"><div className={home.LinkedIn}>LinkedIn</div></a>
+        <a href= "https://github.com/ch-teng" target="_blank"><div className={home.GitHub}>GitHub</div></a>
+        <Link to="/resume"><div className={home.Resume}>Resume</div></Link>
+        <div className={home.PersonalEmail} onClick={() => {navigator.clipboard.writeText("Chris.R.Teng@gmail.com"); alert("Chris.R.Teng@gmail.com has been copied to clipboard")}}>
+          <span className={home.DisplayedEmail}>Personal Email</span> 
+          <span className={home.EmailClicked}>Copied To Clipboard!</span>
           </div>
         
       </div>
